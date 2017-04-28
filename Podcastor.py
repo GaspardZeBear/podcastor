93a2db9817de1da9396140975906c610ce416174
GaspardZeBear <gaspardzebear@hotmail.fr>
2017-04-28 19:50:22 +0200

    List

--- Podcastor.py
+++ Podcastor.py
@@ -6,44 +6,69 @@ import re
 import sys
 import os
 
-url='http://radiofrance-podcast.net/podcast09/rss_15537.xml'
-url='http://radiofrance-podcast.net/podcast09/rss_10351.xml'
-url='http://radiofrance-podcast.net/podcast09/rss_14486.xml'
-url='http://radiofrance-podcast.net/podcast09/rss_11495.xml'
-url='http://cdn1-europe1.new2.ladmedia.fr/var/exports/podcasts/sound/qui-vive.xml'
-url='http://radiofrance-podcast.net/podcast09/rss_14007.xml'
-url='http://radiofrance-podcast.net/podcast09/rss_11475.xml'
-url='http://radiofrance-podcast.net/podcast09/rss_10467.xml'
-url='http://radiofrance-podcast.net/podcast09/rss_16274.xml'
-url='http://radiofrance-podcast.net/podcast09/rss_11921.xml'
-url='http://www.rtl.fr/podcast/100-live.xml'
+urls=list()
+urls.append('http://radiofrance-podcast.net/podcast09/rss_15537.xml')
+urls.append('http://radiofrance-podcast.net/podcast09/rss_10351.xml')
+urls.append('http://radiofrance-podcast.net/podcast09/rss_14486.xml')
+urls.append('http://radiofrance-podcast.net/podcast09/rss_11495.xml')
+urls.append('http://cdn1-europe1.new2.ladmedia.fr/var/exports/podcasts/sound/qui-vive.xml')
+urls.append('http://radiofrance-podcast.net/podcast09/rss_14007.xml')
+urls.append('http://radiofrance-podcast.net/podcast09/rss_11475.xml')
+urls.append('http://radiofrance-podcast.net/podcast09/rss_10467.xml')
+urls.append('http://radiofrance-podcast.net/podcast09/rss_16274.xml')
+urls.append('http://radiofrance-podcast.net/podcast09/rss_11921.xml')
+urls.append('http://www.rtl.fr/podcast/100-live.xml')
+urls.append('http://radiofrance-podcast.net/podcast09/rss_10192.xml')
 
+url='http://radiofrance-podcast.net/podcast09/rss_10192.xml'
+#----------------------------------------------------------------
 def getNum(text) :
   part=re.search('\(\d+/\d+\)',text)
   if part is None :
     part=re.search('\d+/\d+',text)
   #print part
   return part
-  
-  
 
+#----------------------------------------------------------------
+def documentInfo(url) :
+    tree = ET.ElementTree(file=urllib2.urlopen(url))
+    root=tree.getroot()
+    el=tree.iter(tag='channel').next()
+    title=el.find('title').text.encode('utf-8')
+    link=el.find('link').text.encode('utf-8')
+    return( title + ':' + link)
+
+#----------------------------------------------------------------
+def documentsInfo() :
+  for i in range(0,len(urls)) :
+    print(str(i) + " - " + documentInfo(urls[i]))
+  
+#----------------------------------------------------------------
 prefix='podcast'
 filter='.*'
-if len(sys.argv) > 1 and sys.argv[1] is not None :
-  filter=sys.argv[1]
-  if '=' in sys.argv[1] :
-    filter,prefix=sys.argv[1].split('=')
+
+if len(sys.argv) == 1 :
+  documentsInfo()
+  sys.exit()
+
+if len(sys.argv) >= 2 :
+  url=urls[int(sys.argv[1])]
+
+filter='.*'
+if len(sys.argv) >= 3 :
+  filter=sys.argv[2]
+  if '=' in sys.argv[2] :
+    filter,prefix=sys.argv[2].split('=')
   
 download=False
-if len(sys.argv) > 2 and sys.argv[2] is not None :
-  if sys.argv[2] == 'download':
+if len(sys.argv) >= 4 :
+  if sys.argv[3] == 'download':
     download=True
 
-
 tree = ET.ElementTree(file=urllib2.urlopen(url))
-
 root=tree.getroot()
 count=0
+print(documentInfo(url))
 for el in tree.iter(tag='item'):
   text=el.find('title').text.encode('utf-8')
   mp3=el.find('enclosure').attrib['url']
